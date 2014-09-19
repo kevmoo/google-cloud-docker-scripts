e1a6e538341ada6c3a4f7ea29d104036c42e0828
Kevin Moore <github@j832.com>
2014-09-19 12:47:25 -0700

    updated for latest container options, apis, docker image updates

--- delete_gcloud_image.sh
+++ delete_gcloud_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source vars.sh
-gcloud compute firewalls delete ${VM_NAME}-www
+gcloud compute firewall-rules delete ${VM_NAME}-www
 gcloud compute instances delete ${VM_NAME} --zone ${ZONE}
--- init_gcloud_image.sh
+++ init_gcloud_image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source vars.sh
-gcloud compute firewalls create ${VM_NAME}-www\
+gcloud compute firewall-rules create ${VM_NAME}-www\
   --allow tcp:80 --target-tags ${VM_NAME}
 
 gcloud compute instances create ${VM_NAME} \
   --tags ${VM_NAME} \
   --zone ${ZONE}\
   --machine-type f1-micro \
-  --image container-vm-v20140624\
+  --image container-vm-v20140826\
   --image-project google-containers\
   --metadata-from-file google-container-manifest=container_manifest.yaml
 
